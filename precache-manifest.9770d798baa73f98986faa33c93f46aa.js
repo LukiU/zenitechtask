2c3f5ab5d882351cff4e1021e72242f983e33689
LukiU <l.pangonis@gmail.com>
2019-03-31 14:34:16 +0300

    Updates

---
+++ precache-manifest.9770d798baa73f98986faa33c93f46aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d412d29ab565a1535ac6",
+    "url": "/zenitechtask/static/css/main.ed963ca6.chunk.css"
+  },
+  {
+    "revision": "d412d29ab565a1535ac6",
+    "url": "/zenitechtask/static/js/main.0c4a0880.chunk.js"
+  },
+  {
+    "revision": "afdfffae743c425d51e2",
+    "url": "/zenitechtask/static/js/runtime~main.4e8f06d5.js"
+  },
+  {
+    "revision": "6aefb2953b03001638f8",
+    "url": "/zenitechtask/static/js/2.52fbb6ca.chunk.js"
+  },
+  {
+    "revision": "367472929d6bc7c5828d420a24f7c5b7",
+    "url": "/zenitechtask/index.html"
+  }
+];
